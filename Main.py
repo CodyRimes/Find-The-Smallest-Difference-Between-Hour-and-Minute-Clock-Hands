79d582ab96ffc2b65b87d67ac0549b586da72d63
unknown <Cody_Rimes@yahoo.com>
2025-01-31 18:42:32 -0800

    Pushing some code I started while on the desktop. Just some basic input capturing code for the user to input the time of the clock they want to calculate the shortest difference in angle on

---
+++ Main.py
@@ -0,0 +1,8 @@
+#This program will take in the users input for the hour hand and minute hand (by the time on the clock)
+#And will calculate shortest difference in distance by degrees between the two hands (there would be two ways to measure the distance in degrees on a clock, you could go around the long way, or go the shorter way between the two hands)
+
+
+hourHandInputResponse = input("Please enter the time on your clock. First we will take the input of the hour hand: ")
+print(f"The number we received was, {hourHandInputResponse}")
+
+print("Please enter the input for the minute hand")
