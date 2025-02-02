e566c6e4e8170220cdb230252a84a32908578322
unknown <Cody_Rimes@yahoo.com>
2025-02-02 14:13:29 -0800

    Put in more logic into my comments and coded some of it up. Probem is still not complete

--- Main.py
+++ Main.py
@@ -4,10 +4,10 @@
 
 hourHandInputResponse = input("Please enter the time on your clock. First we will take the input of the hour hand: ")
 #Python F statements (note the F before the quotation marks begin) allow us to print variables within a print function/statement, thus allowing the print statement to be dynamic, and not hard coded/static
-print(f"The number we received was, {hourHandInputResponse}")
+print(f"The number we received was: {hourHandInputResponse}")
 
 minuteHandInputResponse = input("Please enter the input for the minute hand on your clock: ")
-print(f"The number we received was, {minuteHandInputResponse}")
+print(f"The number we received was: {minuteHandInputResponse}")
 
 #This function calculates the smallest difference in degrees between the hour hand and the minute hand
 #Receives the time input for the hour hand and the minute hand
@@ -22,3 +22,14 @@ def calculateSmallestDifferenceInDegreesBetweenMinuteAndHourHandOnTheClock(hourH
     #360 degrees / 60 minutes = 6 degrees is moved with each passing minute, i.e. 6 degrees per minute
     #While the minute hand moves faster than the hour hand, we know that the hour hand should be moving even slower (even less degrees moved per minute), thus we need to divide the 60 minutes minutes by each hour
     #360 degree / 60 minutes / 12 hours, or said another way, 360 degrees / 60 minutes = 6 degrees per minute; 6 degrees per minute / 12 hours = 0.5 degrees moved per minute for the hour hand
+    #Now that we know how many degrees each hand moves per minute (The minute hand moves 6 degrees for every minute, and the hour hand moves 0.5 degrees for every minute), we can code it up
+
+    #We will need to multiply the degree per minute by the amount of minutes we have received from user input
+    PositionInDegreesMovedByMinuteHand = (360/60) * minuteHandInputResponse
+
+    PositionInDegreesMovedByHourHand = (((360/60)/12) * hourHandInputResponse)
+
+    print(f"The minute time for the minute hand is at:  {minuteHandInputResponse}. We have calculated the position it is in in degrees is: {PositionInDegreesMovedByMinuteHand}.")
+    print(f"The hour time for the hour hand is at: {hourHandInputResponse}. We have calculated the position it is in in degrees is: {PositionInDegreesMovedByHourHand}.")
+
+calculateSmallestDifferenceInDegreesBetweenMinuteAndHourHandOnTheClock(hourHandInputResponse, minuteHandInputResponse)
